95173ab5508c98a68f3852b26e42408e9df049e7
lehongquan15a <lehongquan15a@gmail.com>
2021-07-05 18:10:15 +0700

    setup sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>
+      <h1 className="hello-word">
         Hello World
       </h1>
     </div>
